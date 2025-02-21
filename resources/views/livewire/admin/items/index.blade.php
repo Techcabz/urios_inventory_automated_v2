0f9c0d6cb6b010c3da244672a8ac26277f398543
Techcabz <techcabz@gmail.com>
2025-02-21 17:20:05 +0800

    master

--- resources/views/livewire/admin/items/index.blade.php
+++ resources/views/livewire/admin/items/index.blade.php
@@ -1,3 +1,29 @@
-<div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+<div class="col-xl-12 col-sm-6 mb-xl-0 mb-4">
+    @include('shared.offline')
+    <div class="card">
+        @include('livewire.admin.items.modal')
+        <div class="card-body p-3">
+            <h4 class="card-title">
+                <div class="d-flex justify-content-between align-items-center">
+                    <h5 class="m-0 font-weight-bold text-muted">ITEMS MANAGEMENT</h5>
+                    <button class="btn bg-gradient-primary" data-bs-toggle="modal" data-bs-target="#addCategoryModal"><i
+                            class="fa fa-plus-square"></i>&nbsp;ADD NEW ITEM</button>
+                </div>
+            </h4>
+            <div class="table-responsived">
+                <table id="datatable" class="table table-borderless">
+                    <thead class="bg-gradient-primary text-white">
+                        <tr>
+                            <th style="width:50px">#</th>
+                            <th>Name</th>
+                            <th style="width:160px">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                   
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
